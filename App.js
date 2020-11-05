ee4e1daac60505dd32d2fc1d3ab28cf6f4440c42
gerryS2xda <Qwerty96xda>
2020-11-05 22:51:12 +0100

    Update App.js for use <NavigatorContainer>

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
+/* 
+    install this dependency before run: 
+    1. npm install react-navigation
+    2. npm install @react-navigation/native 
+    3. npm install @react-navigation/drawer
+    4. npm install react-navigation-stack
+    Swipe right to open
+*/
+
 import { createAppContainer } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from 'react-navigation-stack';
 import LoginScreen from "./src/screen/Login"
 import HomeHostScreen from "./src/screen/Home_host"
@@ -6,6 +16,45 @@ import RegPersonalData from "./src/screen/RegistrazioneDatiPersonali"
 import RegResidenceData from "./src/screen/RegistrazioneDatiResidenza"
 import Inserisci_prenotazione from "./src/screen/Inserisci_prenotazione"
 
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+           name="Home"
+           component={LoginScreen}
+           options={{ headerShown: false }}
+          />
+          <Stack.Screen
+           name="HomeHost"
+           component={HomeHostScreen}
+           options={{ title: 'Home Host' }}
+          />
+          <Stack.Screen
+           name="RegistrationPersonalData"
+           component={RegPersonalData}
+           options={{ title: 'Registrazione' }}
+          />
+          <Stack.Screen
+           name="RegistrationResidenzaData"
+           component={RegResidenceData}
+           options={{ title: 'Registrazione' }}
+          />
+          <Stack.Screen
+           name="Inserisci"
+           component={Inserisci_prenotazione}
+           options={{ title: 'Prenotazione' }}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+/*
 const navigator = createStackNavigator(
   {
     Home: LoginScreen,
@@ -19,3 +68,4 @@ const navigator = createStackNavigator(
   }
 );
 export default createAppContainer(navigator);
+*/
