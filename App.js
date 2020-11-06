ae3b25efa4e65edfc6effad925fac1ccad60e455
RaimondoRanaldo <rranaldo@hotmail.it>
2020-11-06 13:08:49 +0100

    add comment dependencies in App.js

--- App.js
+++ App.js
@@ -2,7 +2,12 @@
     install this dependency before run: 
     1. npm install @react-navigation/native @react-navigation/stack 
     2. npm install @react-navigation/drawer
-    . npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+    3. npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+    4. npm install react-native-calendar-picker
+    5. npm install react
+    6. npm install react native
+    7. npm install react-native-picker-select
+    8. npm install moment
     Swipe right to open
 */
 
