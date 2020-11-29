ce3b77a813715004f57a3cc2bcce34b85afee7a1
gerryS2xda <Qwerty96xda>
2020-11-29 18:49:19 +0100

    Create setupFirestoreDB.js for initialize DB on Cloud Firestore

---
+++ src/firebase/setupFirestoreDB.js
@@ -0,0 +1,14 @@
+import {firebase} from '../config'
+import * as GuestModel from './datamodel/GuestModel'
+
+const setupFirestoreDB = () => {
+
+    //Creazione "guest" collection
+    GuestModel.createGuestDocument("myuid", "dfer24dfeer33tefdrr4", "Laucella", "Gerardo", "13/08/1996", "Avellino", 1234567890, 123450, {}, false);
+    GuestModel.createCreditCardDocumentGuest("myuid", 12344, 123, "Gerardo", "12/2023");
+
+
+}
+
+export default setupFirestoreDB;
+
