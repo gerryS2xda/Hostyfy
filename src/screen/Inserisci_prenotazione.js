73723b590983ad50d45f87adb4c5bc77ca129554
gerryS2xda <Qwerty96xda>
2021-01-20 19:30:43 +0100

    Aggiunta ComboBox in InserisciPrenotazioneScreen, sistemazione elementi della pagina

--- src/screen/Inserisci_prenotazione.js
+++ src/screen/Inserisci_prenotazione.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
-import {View, Text, TextInput, StyleSheet,TouchableOpacity, ScrollView, Alert } from 'react-native'
-import RNPickerSelect from 'react-native-picker-select';
+import React, { useState, useCallback, useRef } from 'react'
+import {View, Text, TextInput, StyleSheet, ScrollView, Alert } from 'react-native'
+import { useIsFocused, useFocusEffect } from '@react-navigation/native';
 import HeaderBar from '../components/CustomHeaderBar';
 import CustomButton from "../components/CustomButton";
 import DatePickerInputField from "../components/DatePickerInputField";
 import * as PrenotazioneModel from "../firebase/datamodel/PrenotazioneModel"; 
+import * as StrutturaModel from "../firebase/datamodel/StrutturaModel";
+import * as AlloggioModel from "../firebase/datamodel/AlloggioModel";
+import * as GuestModel from "../firebase/datamodel/GuestModel";
+import {Dropdown} from 'sharingan-rn-modal-dropdown';
 
 //npm install react-native-picker-select per la combo box
 
@@ -40,7 +44,7 @@ const styles = StyleSheet.create({
 	},
 
 	bottomContainer: {
-		marginTop: "40%",
+		marginTop: "15%",
 		marginBottom: 30,
 	},
 
@@ -66,76 +70,84 @@ const styles = StyleSheet.create({
 
 
 const Inserisci_prenotazione = ({route, navigation}) => {
+	
+	//Dichiarazione variabili
 	const {user} = route.params;
-	const [struttura, setStruttura] = useState('');
-	const [alloggio, setAlloggio] = useState('');
-	const [nome, setNome] = useState('');
-	const [cognome, setCognome] = useState('');
+	const [strutturaId, setStrutturaId] = useState('');
+	const [struttureList, setStruttureList] = useState([]);
+	const [isAlloggioDropDisabled, setAlloggioDropDisabled] = useState(true);
+	const [alloggiList, setAlloggiList] = useState([]);
+	const [alloggioId, setAlloggioId] = useState('');
 	const [numTel, setNumTelefono] = useState(0);
 	const [numPers, setNumeroPersone] = useState(0);
 	const [email, setEmail] = useState('');
 	const [costo, setCosto] = useState(0);
 	const [dateStart, setDateStart] = useState("");
 	const [dateEnd, setDateEnd] = useState("");
-	
-	const pickerStyle = {
-		inputIOS: {
-			paddingHorizontal: 10,
-			borderBottomWidth: 1,
-			borderColor: '#cc3881',
-			height:40,
-			width:"100%",
-			alignItems: 'center',
-			marginTop: 16,
-			fontFamily: "MontserrantSemiBold",
-			paddingLeft: 9,
-		},
-		placeholder: {
-			fontFamily: "MontserrantSemiBold",
-			paddingLeft: 9,
-		  },
-		inputAndroid: {
-			paddingHorizontal: 10,
-			borderBottomWidth: 1,
-			borderColor: '#cc3881',
-			height:40,
-			width:"100%",
-			alignItems: 'center',
-			color:'#000000',
-			marginTop: 16,
-			fontFamily: "MontserrantSemiBold",
-			paddingLeft: 9,
-		},
+	const isFocused = useIsFocused();
+
+	//Variabili per 'useRef'
+	const numTelRef = useRef(null);
+	const numPersRef = useRef(null);
+	const emailRef = useRef(null);
+	const costoRef = useRef(null);
+
+	//Caricamento dei dati non appena inizia il rendering dell'applicazione
+	useFocusEffect(
+        useCallback(() => {
+          // Do something when the screen is focused
+			async function getStruttureData(){
+				var itemList = [];
+				var struttureDocs = await StrutturaModel.getAllStrutture();
+				if(struttureDocs.length == 0){
+					setStruttureList(itemList);
+				}else{
+					for(const doc of struttureDocs){
+						var struttura = doc.data();
+						var oggetto = {
+							value: doc.id, 
+							label: struttura.denominazione,
+						}
+						itemList.push(oggetto);
+					}
+					setStruttureList(itemList);
+				}
+			}
+            getStruttureData();
+          return () => {
+            // Do something when the screen is unfocused
+            // Useful for cleanup functions
+          };
+        }, [isFocused])
+      );
+
+	//Caricamento dei dati relativi ad un alloggio non appena viene scelta una struttura
+	const onChangeStrutturaDropDown = (value) => {
+		setStrutturaId(value);
+		async function getAlloggiData(){
+			var itemList = [];
+			var alloggiDocs = await AlloggioModel.getAllAlloggiOfStruttura(value);
+			if(alloggiDocs.length == 0){
+				setAlloggiList(itemList);
+			}else{
+				for(const doc of alloggiDocs){
+					var alloggio = doc.data();
+					var oggetto = {
+						value: doc.id, 
+						label: alloggio.nomeAlloggio
+					}
+					itemList.push(oggetto);
+				}
+				setAlloggiList(itemList);
+				setAlloggioDropDisabled(false);
+			}
+		}
+		getAlloggiData();
 	};
-
-	const createPositiveAlert = () =>
-      Alert.alert(
-      "Inserisci prenotazione",
-      "La nuova prenotazione e' stata memorizzata con successo!",
-      [
-          {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-          },
-          { text: "OK", onPress: () => navigation.navigate('HomeHost') }
-      ],
-      { cancelable: false }
-	);
-
-	const createNegativeAlert = (msgError) =>
-      Alert.alert(
-      "Inserisci prenotazione", msgError,
-      [
-          {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-          },
-          { text: "OK", onPress: () => console.log("Ok Pressed") }
-      ],
-      { cancelable: false }
-	);
+	
+	const onChangeAlloggioDropDown = (value) => {
+		setAlloggioId(value);
+	};
 	
   	return(
 	<View style={styles.maincontainer}>
@@ -143,44 +155,20 @@ const Inserisci_prenotazione = ({route, navigation}) => {
 		<ScrollView style={styles.bodyScrollcontainer}>
             <View style={styles.scrollContent}> 
 				<View style = {styles.topContainer}>
-					<RNPickerSelect
-						style = {pickerStyle}
-						onValueChange = {(struttura) => {setStruttura(struttura);}}
-						value={struttura}
-						placeholder = {{
-							label: 'Struttura',
-							value: "Struttura",
-						}}
-						items={[
-							{ label: 'Posillipo', value: 'Posillipo' },
-							{ label: 'Margellina', value: 'Margellina' },
-						]}
-						useNativeAndroidPickerStyle={false}
+					<Dropdown
+						label="Struttura"
+						data={struttureList}
+						enableSearch
+						value={strutturaId}
+						onChange={onChangeStrutturaDropDown}
 					/>
-					<RNPickerSelect	
-						style = {pickerStyle}
-						onValueChange = {(alloggio) => {setAlloggio(alloggio);}}
-						value={alloggio}
-						placeholder = {{
-							label: 'Alloggio',
-							value: "Alloggio",
-						}}
-						items={[
-							{ label: 'Mare chiaro', value: 'Mare chiaro' },
-							{ label: 'Vietri', value: 'Vietri' },
-						]}
-						
-						useNativeAndroidPickerStyle={false}
-					/>
-					<TextInput
-						style = {styles.singleTextInput}
-						placeholder = "Nome"
-						onChangeText = {(nome) => setNome(nome)}
-					/>
-					<TextInput
-						style = {styles.singleTextInput}
-						placeholder = "Cognome"
-						onChangeText = {(cognome) => setCognome(cognome)}
+					<Dropdown
+						label="Alloggio"
+						data={alloggiList}
+						enableSearch
+						disabled={isAlloggioDropDisabled}
+						value={alloggioId}
+						onChange={onChangeAlloggioDropDown}
 					/>
 				</View>
 				<View style = {styles.middleUpperContainer}>
@@ -203,11 +191,13 @@ const Inserisci_prenotazione = ({route, navigation}) => {
 				</View>
 				<View style = {styles.middleLowerContainer}>
 					<TextInput
+						ref = {numPersRef}
 						style = {[styles.middleTextInput, {width: "35%"}]}
 						placeholder = "N. persone"
 						onChangeText = {(numPers) => setNumeroPersone(numPers)}
 					/>
 					<TextInput
+						ref = {numTelRef}
 						style = {[styles.middleTextInput, {width: "55%"}]}
 						placeholder = "N. telefono"
 						onChangeText = {(numTel) => setNumTelefono(numTel)}
@@ -215,6 +205,7 @@ const Inserisci_prenotazione = ({route, navigation}) => {
 				</View>
 				<View style={styles.normalContainer}>
 					<TextInput
+						ref = {costoRef}
 						style = {styles.singleTextInput}
 						placeholder = "Costo"
 						onChangeText = {(costo) => setCosto(costo)}
@@ -222,8 +213,9 @@ const Inserisci_prenotazione = ({route, navigation}) => {
 				</View>
 				<View style={styles.normalContainer}>
 					<TextInput
+						ref = {emailRef}
 						style = {styles.singleTextInput}
-						placeholder = "Email"
+						placeholder = "Email dell'ospite"
 						onChangeText = {(email) => setEmail(email)}
 					/>
 				</View>
@@ -231,10 +223,61 @@ const Inserisci_prenotazione = ({route, navigation}) => {
 					<CustomButton 
 						nome="Inserisci" 
 						styleBtn={{width: "100%"}}
-						onPress={()=>{
-							PrenotazioneModel.createPrenotazioniDocument(costo, dateStart, dateEnd, email, numPers, numTel, struttura, alloggio, "chiave" + alloggio, user.userId);
-							createPositiveAlert();
-							//createNegativeAlert("Impossibile memorizzare la prenotazione!!");
+						onPress={()=>{ 
+							
+							// Ottieni riferimento dell'utente guest
+							async function createAndSavePrenotazione(){
+								
+								// Ottieni riferimento dell'utente guest mediante l'email
+								var guestDocs = await GuestModel.getGuestDocumentByEmail(email);
+								if(guestDocs.length == 0){
+									Alert.alert(
+										"Nuova prenotazione",
+										"Non è possibile memorizzare la nuova prenotazione perche' l'utente non utilizza Hostyfy!",
+										[
+											{
+											text: "Cancel",
+											onPress: () => console.log("Cancel Pressed"),
+											style: "cancel"
+											},
+											{ text: "OK", onPress: () => navigation.navigate('HomeHost') }
+										],
+										{ cancelable: false }
+									);
+								}else{
+									for(const doc of guestDocs){
+										var guest = doc.data();
+										await PrenotazioneModel.createPrenotazioniDocument(user.userIdRef, guest.userId, strutturaId, alloggioId, dateStart, dateEnd, email, numPers, numTel, costo);
+										
+										//Resetta i campi
+										numTelRef.current.clear();  
+										numPersRef.current.clear();
+										costoRef.current.clear();
+										emailRef.current.clear();
+										setDateStart("");
+										setDateEnd("");
+										setStrutturaId("");
+										setAlloggioId("");
+										setAlloggioDropDisabled(true);
+										
+
+										Alert.alert(
+											"Nuova prenotazione",
+											"La nuova prenotazione e' stata memorizzata con successo!",
+											[
+												{
+												text: "Cancel",
+												onPress: () => console.log("Cancel Pressed"),
+												style: "cancel"
+												},
+												{ text: "OK", onPress: () => navigation.navigate('HomeHost') }
+											],
+											{ cancelable: false }
+										);
+									}
+								}
+							}
+							createAndSavePrenotazione();
 						}} 
 					/>
 				</View>
@@ -244,4 +287,5 @@ const Inserisci_prenotazione = ({route, navigation}) => {
   );
 }
 
-export default Inserisci_prenotazione
+export default Inserisci_prenotazione;
+
