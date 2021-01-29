f087f517fad6c1950d0ce9495c828696cd837b9f
raimondoranaldo <rranaldo@hotmail.it>
2021-01-29 13:58:15 +0100

    Ristrutturazione della grafica dell'intera entry di una descrizione

--- src/components/CustomRowGeneralMieChiavi.js
+++ src/components/CustomRowGeneralMieChiavi.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        padding: 10,
         marginLeft:"4%", //oldvalue: 16
         marginRight:"4%", //oldvalue: 16
         marginTop: "2%", //oldvalue: 8
-        marginBottom: "3%", //oldvalue: 8
-        borderRadius: 3,
-        elevation: 2,
+        borderRadius: 18,
+        borderWidth: 3,
         width: "93%",
-        //backgroundColor: 'black'
+        borderColor: "#e4eded",
+        height: 80
     },
     title: {
         fontSize: 16,
@@ -24,15 +24,19 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         marginLeft: 12,
         justifyContent: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+        justifyContent: 'center',
     },
     description: {
         fontSize: 12,
         fontFamily: 'MontserrantItalic',
     },
     photo: {
-        height: 50,
-        width: 50,
-        borderRadius: 4,
+        borderTopLeftRadius: 16,
+        borderBottomLeftRadius:16,
+        height: "100%",
+        width: 70,
     },
     arrow: {
         alignSelf: 'center',
@@ -61,10 +65,7 @@ const CustomRowGeneralAlloggio = (props) => {
                         {props.description}
                     </Text>
                 </View>
-                <Image
-                        source = {require('../../assets/arrow.png')}
-                        style = {styles.arrow} 
-                    />
+                <Icon name={"chevron-right"} color={"#000000"} size={40} style={styles.arrow} />
             </View>
         </TouchableOpacity>
     );
